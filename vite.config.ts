5a5704bbc391d1a8ed2cf372562c9161a953a29b
Aditya Fakhri Riansyah <adityafakhri03@gmail.com>
2025-12-10 14:02:24 +0700

    feat: Add PWA support by integrating `vite-plugin-pwa`, configuring manifest, and including new icons and favicon.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath } from 'url';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['catetyuk2.ico', 'robots.txt'],
+      manifest: {
+        name: 'CatetYuk',
+        short_name: 'CatetYuk',
+        description: 'CatetYuk - simplify your task',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'catetyuk2.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'catetyuk2.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
